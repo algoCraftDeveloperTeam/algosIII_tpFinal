75dd88d660f39404fe517a38f39cf3636b1f923d
mporto <manu.porto94@gmail.com>
2015-05-30 11:52:35 -0300

    Se agrega clase UnitInConstruction. Se agregan test UnitInConstructionTest.

---
+++ src/fiuba/algo3/units/UnitInConstruction.java
@@ -0,0 +1,18 @@
+package fiuba.algo3.units;
+
+/**
+ * Created by mporto on 30/05/15.
+ */
+public class UnitInConstruction {
+    private Unit unitToBeBuilt;
+    private int remainingTurnsToBeBuilt;
+
+    public UnitInConstruction(Unit unit) {
+        unitToBeBuilt = unit;
+        remainingTurnsToBeBuilt = unit.getConstructionTime();
+    }
+
+    public int getRemainingTurns() {
+        return remainingTurnsToBeBuilt;
+    }
+}
---
+++ test/fiuba/algo3/units/UnitInConstructionTest.java
@@ -0,0 +1,20 @@
+package fiuba.algo3.units;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mporto on 30/05/15.
+ */
+public class UnitInConstructionTest {
+
+    @Test
+    public void testConstructionTimeEqualsToTheOneOfTheUnit() throws Exception {
+
+        Marine aMarine = new Marine();
+        UnitInConstruction unitToBeBuilt = new UnitInConstruction(aMarine);
+
+        assertEquals(unitToBeBuilt.getRemainingTurns(), aMarine.getConstructionTime());
+    }
+}
