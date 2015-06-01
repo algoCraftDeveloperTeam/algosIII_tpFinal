e8cf33b9a7bdc64f0f440a8c5f7d77b882191e39
Ignacio Sueiro <iasueiro@gmail.com>
2015-06-01 00:53:29 -0300

    Se agrega MineralNexus con sus respectivos tests

---
+++ test/fiuba/algo3/buildings/MineralNexusTest.java
@@ -0,0 +1,36 @@
+package fiuba.algo3.buildings;
+
+import fiuba.algo3.player.Player;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nsueiro on 01/06/15.
+ */
+public class MineralNexusTest {
+    private MineralNexus mn;
+    private Player p;
+    @Before
+    public void setUp(){
+        this.p = new Player();
+        this.mn = new MineralNexus(p);
+    }
+
+    @Test
+    public void testGetVitality() {
+        Assert.assertEquals(250, mn.getVitality());
+    }
+
+    @Test
+    public void testGetShield() {
+        Assert.assertEquals(250, mn.getShield());
+    }
+
+    @Test
+    public void testMineralNexusAddsMineralsToOwnersStorage() {
+        mn.addToPlayerStorage();
+        Assert.assertEquals(210, p.getMineralStorage());
+    }
+
+}
