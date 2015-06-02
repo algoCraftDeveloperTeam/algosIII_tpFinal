213033f666810b479bf44934fb07b347957efc37
mtejedor <mtejedor@hexacta.com>
2015-06-02 00:30:50 -0300

    clase Access con tests, 100% cobertura

---
+++ src/fiuba/algo3/buildings/Access.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.buildings;
+
+import fiuba.algo3.gameVariables.Cost;
+import fiuba.algo3.gameVariables.Life;
+import fiuba.algo3.player.Player;
+
+public class Access extends Building{
+
+    public Access(Player p) {
+    	this.owner = p;
+        this.constructionCost = new Cost(150, 0);
+        this.constructionTime = 8;
+        this.life = new Life(500, 500);
+    }
+
+}
---
+++ test/fiuba/algo3/buildings/AccessTest.java
@@ -0,0 +1,60 @@
+package fiuba.algo3.buildings;
+
+import fiuba.algo3.player.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class AccessTest {
+
+    private Access aAccess;
+    private Player owner;
+
+    @Before
+    public void setUp() throws Exception {
+        this.owner = new Player();
+        this.aAccess = new Access(owner);
+    }
+
+    @Test
+    public void testIsEarthBuildable() {
+        Assert.assertTrue(aAccess.isEarthBuildable());
+    }
+
+    @Test
+    public void testIsGasBuildable() {
+        Assert.assertFalse(aAccess.isGasBuildable());
+    }
+
+    @Test
+    public void testIsMineralBuildable() {
+        Assert.assertFalse(aAccess.isMineralBuildable());
+    }
+
+    @Test
+    public void testGetConstructionTime() {
+        Assert.assertEquals(8, aAccess.getConstructionTime());
+    }
+
+    @Test
+    public void testGetVitality() {
+        Assert.assertEquals(500, aAccess.getVitality());
+    }
+
+    @Test
+    public void testGetShield() {
+        Assert.assertEquals(500, aAccess.getShield());
+    }
+
+    @Test
+    public void testGetGasCost() {
+        Assert.assertEquals(0, aAccess.getConstructionCost().getGasCost());
+    }
+
+    @Test
+    public void testGetMineralCost() {
+        Assert.assertEquals(150, aAccess.getConstructionCost().getMineralCost());
+    }
+
+}
