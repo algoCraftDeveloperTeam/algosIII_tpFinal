f671e103363eef55171653f3e1fc53ec307ef59b
mporto <manu.porto94@gmail.com>
2015-06-04 12:46:39 -0300

    Ahora Goliath y Marine heredan de CombatUnit.
    Se agregan tests de combate entre unidades de tierra.

--- src/fiuba/algo3/units/Goliath.java
+++ src/fiuba/algo3/units/Goliath.java
@@ -1,12 +1,14 @@
 package fiuba.algo3.units;
 
+import fiuba.algo3.gameVariables.AttackRange;
 import fiuba.algo3.gameVariables.Cost;
+import fiuba.algo3.gameVariables.Damage;
 import fiuba.algo3.gameVariables.Life;
 
 /**
  * Created by mporto on 30/05/15.
  */
-public class Goliath extends Unit {
+public class Goliath extends CombatUnit{
     public Goliath(){
         sizeForTransport = 2;
         fieldOfVision = 8;
@@ -14,5 +16,7 @@ public class Goliath extends Unit {
         unitSize = 2;
         life = new Life(125, 0);
         trainingCost = new Cost(100, 50);
+        attackRange = new AttackRange(6, 5);
+        damage = new Damage(12, 10);
     }
 }
---
+++ test/fiuba/algo3/combat/GroundUnitsTest.java
@@ -0,0 +1,41 @@
+package fiuba.algo3.combat;
+
+import fiuba.algo3.units.CombatUnit;
+import fiuba.algo3.units.Goliath;
+import fiuba.algo3.units.Marine;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by mporto on 04/06/15.
+ */
+public class GroundUnitsTest {
+    
+    private CombatUnit marine;
+    private CombatUnit goliath;
+    
+    @Before
+    public void setUp() throws Exception {
+        marine = new Marine(); 
+        goliath = new Goliath();
+    }
+
+    @Test
+    public void testMarineAttacksGoliath() throws Exception {
+        marine.attack(goliath);
+        // 125 (goliath's vitality) - 6 (marine's attack) = 119
+        int expectedRemainingVitality = 119;
+
+        Assert.assertEquals(expectedRemainingVitality, goliath.getVitality());
+    }
+
+    @Test
+    public void testGoliathAttacksMarine() throws Exception {
+        goliath.attack(marine);
+        // 40 (marine's vitality - 12 (goliat's attack) = 28
+        int expectedRemainingVitality = 28;
+
+        Assert.assertEquals(expectedRemainingVitality, marine.getVitality());
+    }
+}
