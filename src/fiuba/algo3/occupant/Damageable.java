4d08e6d372d56ad87d659c41430c5b0c1ca3d7c5
mporto <manu.porto94@gmail.com>
2015-06-05 20:18:18 -0300

    Se agrega la interfaz Damageable.
    Unit:
    - Ahora implementa la interfaz Damageable.

---
+++ src/fiuba/algo3/occupant/Damageable.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.occupant;
+
+import fiuba.algo3.gameVariables.Damage;
+
+/**
+ * Created by mporto on 05/06/15.
+ */
+public interface Damageable {
+
+     void receiveDamage(Damage damage);
+}
