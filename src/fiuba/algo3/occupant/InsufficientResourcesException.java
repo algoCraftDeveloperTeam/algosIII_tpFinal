fa46a195b9948935664098ac98176c190b053363
mporto <manu.porto94@gmail.com>
2015-06-08 10:24:25 -0300

    Se agrega la clase InsufficientResourcesException.

---
+++ src/fiuba/algo3/occupant/InsufficientResourcesException.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.occupant;
+
+/**
+ * Created by mporto on 08/06/15.
+ */
+public class InsufficientResourcesException extends Exception{
+    public InsufficientResourcesException() {
+    }
+}
