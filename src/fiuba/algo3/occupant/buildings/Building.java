89281765285dca99dd62a99560dc7adc0c4f5fbb
Ignacio Sueiro <iasueiro@gmail.com>
2015-06-08 14:17:26 -0300

    Builing y BuildingInConstruction ahora implementan Occupant y Damageable

--- src/fiuba/algo3/occupant/buildings/Building.java
+++ src/fiuba/algo3/occupant/buildings/Building.java
@@ -1,22 +1,27 @@
 package fiuba.algo3.occupant.buildings;
 
 import fiuba.algo3.gameVariables.Cost;
+import fiuba.algo3.gameVariables.Damage;
 import fiuba.algo3.gameVariables.Life;
+import fiuba.algo3.gameVariables.Position;
+import fiuba.algo3.occupant.Damageable;
 import fiuba.algo3.occupant.Occupant;
 import fiuba.algo3.player.Player;
 
 /**
  * Created by mporto on 30/05/15.
  */
-public abstract class Building implements Occupant {
+public abstract class Building implements Occupant, Damageable {
 
     Cost constructionCost;
     int constructionTime;
     Life life;
     Player owner;
+    Position position;
 
     public Building(Player player) {
         this.owner = player;
+        this.position = new Position(0, 0);
     }
     /*
     public Building(){
@@ -62,5 +67,14 @@ public abstract class Building implements Occupant {
     public boolean allowBuildTemplarArchives(){
         return false;
     }
+    public void setPosition(int x, int y){
+        position.move(x, y);
+    }
+    public void receiveDamage(Damage damage){
+        life.receiveAttack(damage.getGroundDamage());
+    }
 
+    public Position getPosition(){
+        return position;
+    }
 }
