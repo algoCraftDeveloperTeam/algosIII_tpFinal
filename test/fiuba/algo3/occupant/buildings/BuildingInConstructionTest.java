f36c167862ccc13dc871d8565d0393539e244b82
Ignacio Sueiro <iasueiro@gmail.com>
2015-06-09 13:23:54 -0300

    Se eliminan test duplicados

--- test/fiuba/algo3/occupant/buildings/BuildingInConstructionTest.java
+++ test/fiuba/algo3/occupant/buildings/BuildingInConstructionTest.java
@@ -1,13 +1,10 @@
 package fiuba.algo3.occupant.buildings;
 
-import fiuba.algo3.occupant.InsufficientResourcesException;
 import fiuba.algo3.player.Player;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by mporto on 30/05/15.
@@ -48,24 +45,10 @@ public class BuildingInConstructionTest {
     }
 
     @Test
-    public void testTryingToBuildWithEnoughResourcesDoesNotThrowAnException() {
-        assertTrue(true);
-    }
-
-    @Test
     public void testBuildingSubstractsResourcesFromPlayer() {
         int gasResourcesLeft = this.player.getGasStorage();
         int mineralResourcesLeft = this.player.getMineralStorage();
         assertTrue(gasResourcesLeft == 0 && mineralResourcesLeft == 150);
     }
-
-    @Test (expected = InsufficientResourcesException.class)
-    public void testTryingToBuildWithNotEnoughResourcesThrowsAnException() throws InsufficientResourcesException {
-        Player player2 = new Player();
-        player2.substractMinerals(175);
-        Building building2 = new MineralNexus(player2);
-        BuildingInConstruction inConstruction = new BuildingInConstruction(building2);
-        assertTrue("Trying to build without enough resources didn't throw an exception", false);
-    }
-
+    
 }
