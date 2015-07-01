fb890a94625015e65dff1fc83a32b52ef49b8bcd
Ignacio Sueiro <iasueiro@gmail.com>
2015-06-30 23:46:18 -0300

    Se agrega lógica para el correcto uso del StarPort y la nave de tranporte

--- src/fiuba/algo3/model/occupant/buildings/StarPort.java
+++ src/fiuba/algo3/model/occupant/buildings/StarPort.java
@@ -1,14 +1,19 @@
 package fiuba.algo3.model.occupant.buildings;
 
+import fiuba.algo3.model.exceptions.InsufficientAvailablePopulationException;
+import fiuba.algo3.model.exceptions.InsufficientResourcesException;
+import fiuba.algo3.model.exceptions.SubtractedResourcesGreaterThanStoragedException;
 import fiuba.algo3.model.gameVariables.Cost;
 import fiuba.algo3.model.gameVariables.Life;
 import fiuba.algo3.model.map.Coordinates;
+import fiuba.algo3.model.occupant.units.TerranTransportVessel;
+import fiuba.algo3.model.occupant.units.UnitInTraining;
 import fiuba.algo3.model.player.Player;
 
 /**
  * Created by nsueiro on 01/06/15.
  */
-public class StarPort extends Building {
+public class StarPort extends UnitCreator {
     
 
     public StarPort(Player player, Coordinates coordinate){
@@ -18,5 +23,14 @@ public class StarPort extends Building {
         this.life = new Life(1300, 0);
         this.constructionTime = 10;
 		this.info = "Tierra: en estos paramos se pueden construir todo tipo de edificios,\nexceptuando los de recoleccion de recursos, \ntodas las unidades pueden caminar sobre ellos.";
+    }
+
+    @Override
+    public void trainUnit() throws InsufficientAvailablePopulationException, InsufficientResourcesException,
+            SubtractedResourcesGreaterThanStoragedException {
+        TerranTransportVessel aTerranTransportVessel = new TerranTransportVessel();
+        this.chargeUnitRequirementsToOwner(aTerranTransportVessel);
+        UnitInTraining aMarineInTraining = new UnitInTraining(aTerranTransportVessel);
+        this.trainingQueue.add(aMarineInTraining);
     }
 }
