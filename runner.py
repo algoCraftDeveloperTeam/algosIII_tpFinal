4c41a42db49751618981ecb9aee9a72836cfcff8
Ignacio Sueiro <iasueiro@gmail.com>
2015-07-01 00:14:39 -0300

    Upsi

--- runner.py
+++ runner.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
+    os.system("ant -f offlinebuild.xml")
+    os.chdir("build/")
     while True:
-        os.system("ant -f offlinebuild.xml")
-        os.chdir("build/")
         os.system("java fiuba.algo3.AlgoCraftMain")
         if raw_input("") != "":
             break
