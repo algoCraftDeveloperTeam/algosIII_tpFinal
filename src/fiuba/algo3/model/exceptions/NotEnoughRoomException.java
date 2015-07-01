865e784e80871d91d801a34c8227479f0b281785
mtejedor <mtejedor@hexacta.com>
2015-07-01 03:33:07 -0300

    error message para NotEnoughRoomException

--- src/fiuba/algo3/model/exceptions/NotEnoughRoomException.java
+++ src/fiuba/algo3/model/exceptions/NotEnoughRoomException.java
@@ -4,4 +4,7 @@ package fiuba.algo3.model.exceptions;
  * Created by nsueiro on 29/06/15.
  */
 public class NotEnoughRoomException extends Exception {
+	public String getMessage(){
+		return "There's no enough room";
+	}
 }
